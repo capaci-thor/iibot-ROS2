618f6a7edfea5e9e9794c0dde205d0c213f50f88
Eduardo Ortega <rotjeot@gmail.com>
2022-09-16 12:30:15 -0500

    try fix

--- src/py_pubsub/setup.py
+++ src/py_pubsub/setup.py
@@ -11,7 +11,7 @@ setup(
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
     ],
-    install_requires=['setuptools'],
+    #install_requires=['setuptools'],
     zip_safe=True,
     maintainer='Eduardo Ortega',
     maintainer_email='rotjeot@gmail.com',
