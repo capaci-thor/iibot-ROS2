82940ba7cb0c680d61cbfe28f12bda5897a64cf2
Eduardo Ortega <rotjeot@gmail.com>
2022-12-12 11:51:31 -0600

    serial port

--- src/py_pubsub/py_pubsub/sub_lyapunov.py
+++ src/py_pubsub/py_pubsub/sub_lyapunov.py
@@ -245,7 +245,8 @@ class MoveSubscriber(Node):
         #control
         self.uref.append( k1*math.cos(self.dseta[self.i])* self.iota[self.i]) 
         try:
-            self.wref.append( k2*self.dseta[self.i] + (k1/self.dseta[self.i]) * (math.cos(self.dseta[self.i]) * math.sin(self.dseta[self.i]) * (self.dseta[self.i] + (q2 * self.psi[self.i])) ))
+            self.wref.append( k2*self.dseta[self.i] + (k1/self.dseta[self.i]) * (math.cos(self.dseta[self.i])
+            * math.sin(self.dseta[self.i]) * (self.dseta[self.i] + (q2 * self.psi[self.i])) ))
         except:
             self.wref.append(0)
 
